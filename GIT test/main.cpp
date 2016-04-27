82aeb942fa3e41318ace6803d85881c326f95685
basvlk <basvlk@yahoo.com>
2016-04-27 16:39:28 -0300

    Initial Commit

---
+++ GIT test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GIT test
+//
+//  Created by Bas Vellekoop on 27/04/2016.
+//  Copyright © 2016 Bas Vellekoop. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
